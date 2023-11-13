65722b43d3e87b5cf295b2d28319e1cef90103f7
jAzz-hub <joaogu.2001@gmail.com>
2023-11-13 14:37:02 -0300

    Consertando funcionalidade de Delete | Fazendo comandos orientados à eventos sob o uso de teclas

--- js/Calc.js
+++ js/Calc.js
@@ -3,25 +3,92 @@ let VALOR="";
 let MemoryStack = Array();
 let MemoryQueue = Array();
 
-// Ideia:
+function doNothing()
+{
 
-    //O que?
-        // Pegar qualquer coisa que eu aperte na interface e jogar dentro de uma variável
-        // para depois rodar um switch case para adicionar eventListener nela
+}
 
-
-    // Por quê?
-        // Dessa forma cria-se um afunilamento para a ordem do usuário 
-        // Torna o código menos verboso e mais elegante
-
-
-
-function handlerEvents(event)
+function handlerEventsClick(event)
 {
     const button = event.target;
     const buttonId = button.getAttribute("value");
     add_in_VALOR(buttonId);
 }
+
+
+document.addEventListener("keydown", handlerEventsKeyboard);
+function handlerEventsKeyboard(event)
+{
+    const key = parseFloat(event.which);
+
+    switch (event.which)
+    {
+
+        case 8:
+            DEL();
+            break;
+
+        case 27:
+            AC();
+            break;
+
+        case 48:
+            add_in_VALOR('0');
+            break;
+        case 49:
+            add_in_VALOR('1');
+            break;
+        case 50:
+            add_in_VALOR('2');
+            break;
+        case 51:
+            add_in_VALOR('3');
+            break;
+        case 52:
+            add_in_VALOR('4');
+            break;
+        case 53:
+            add_in_VALOR(event.key);
+            break;
+        case 54:
+            add_in_VALOR('6');
+            break;
+        case 55:
+            add_in_VALOR('7');
+            break;
+        case 56:
+            add_in_VALOR(event.key);
+            break;
+        case 57:
+            add_in_VALOR('9');
+            break;
+        
+        case 13 || event.key == '=':
+            igual();
+            break;
+
+        case 187:
+            if (event.key == '+')
+            {
+                add_in_VALOR('+');
+            }
+            else
+            {
+                doNothing();
+            }
+            break;
+
+        case 189:
+            add_in_VALOR('-');
+            break;
+
+        case 81 || 191:
+            add_in_VALOR('/');
+            break;
+    }
+
+}
+
 
 // .addEventListener afunila todos os botões da classe números.
 // Assim podemos utilizar handlerEvents para capturar qualquer eventos destes.
@@ -29,24 +96,24 @@ const Number_buttons = document.querySelectorAll(".numeros");
 Number_buttons.forEach(
     (button) => 
     {
-        button.addEventListener("click", handlerEvents);
+        button.addEventListener("click", handlerEventsClick);
     }
 )
 
-function add_in_VALOR(e){
+function add_in_VALOR(e)
+{
     
-    // window.alert(e);
-    // mapa = {e.target: 0};
-
     var numero = document.getElementById("tela").innerHTML;
+
     if(numero=="0"){
-        document.getElementById("tela").innerHTML="0"
+        document.getElementById("tela").innerHTML=""
     }
     document.getElementById("tela").innerHTML+=e;
 }
 
 
-function AC(){
+function AC()
+{
     document.getElementById("tela").innerHTML="0"
     
     if (MemoryQueue.length == 2)
@@ -61,25 +128,38 @@ function AC(){
 }
 document.getElementById("btn_clear").addEventListener("click",AC)
 
-function porcentagem(){
+function porcentagem()
+{
     document.getElementById("tela").innerHTML=(document.getElementById("tela").innerHTML)/100
 }
 document.getElementById("btn_sum").addEventListener("btn_percent",porcentagem)
 
-function DEL(){
+function DEL()
+{
+    let screenText = document.getElementById("tela").innerHTML.split("");
+    
+    screenText.pop();
+    
+    afterDel = screenText.join("");
 
-    document.getElementById('tela').innerHTML=document.getElementById('tela').innerHTML.substring(0,(document.getElementById('tela').innerHTML.length)-1);
-    // document.getElementById("tela").innerHTML=document.getElementByI
-    // d("tela").innerHTML.substring(0,(document.getElementById("tela").innerHTML.length)-1);
+    console.log(typeof afterDel);
+
+    afterDel;
+    document.getElementById("tela").innerHTML = '';
+    document.getElementById("tela").innerHTML += afterDel;
+
+
 }
 document.getElementById("btn_delete").addEventListener("",DEL)
 
-function igual(){
+function igual()
+{
     document.getElementById("tela").innerHTML=eval( document.getElementById("tela").innerHTML).toString();
 }
 document.getElementById("btn_sum").addEventListener("",add_in_VALOR)
 
-function add_in_MEMORY(){
+function add_in_MEMORY()
+{
 
     if(MemoryStack[0] == undefined)
     {
@@ -121,7 +201,8 @@ function add_in_MEMORY(){
 }
 document.getElementById("btn_sumMem").addEventListener("",add_in_MEMORY)
 
-function sub_in_MEMORY(){
+function sub_in_MEMORY()
+{
 
     if(MemoryStack[0] == undefined)
     {
@@ -168,7 +249,8 @@ function sub_in_MEMORY(){
 }
 document.getElementById("btn_subMem").addEventListener("",sub_in_MEMORY)
 
-function clear_in_MEMORY(){
+function clear_in_MEMORY()
+{
     MemoryStack.pop();
 }
 document.getElementById("btn_sum").addEventListener("",add_in_VALOR)
